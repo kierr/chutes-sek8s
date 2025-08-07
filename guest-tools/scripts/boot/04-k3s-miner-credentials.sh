0abf69bb78f095dfb1b51fb554fa929afc98d4b3
Kyle Widmann <kyle.widmann@gmail.com>
2025-08-07 15:11:05 -0400

    Fix miner credentials boot scripts

--- guest-tools/scripts/boot/04-k3s-miner-credentials.sh
+++ guest-tools/scripts/boot/04-k3s-miner-credentials.sh
@@ -3,13 +3,13 @@ set -e
 
 # Log function
 log() {
-    echo "$1" >> /var/log/first-boot-miner-kubeconfig.log
+    echo "$1" >> /var/log/first-boot-miner-credentials.log
 }
 
 log "Loading miner credentials..."
-MINER_SS58=%(cat ~/miner-ss58)
+MINER_SS58=$(cat ~/miner-ss58)
 log "Loaded miner ss58..."
-MINER_SEED=%(cat ~/miner-seed)
+MINER_SEED=$(cat ~/miner-seed)
 log "Loaded miner seed..."
 
 log "Creating miner credentials secret..."
